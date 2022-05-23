b1a7ac30755f6d8c692f760a650d42baa08868dd
Oksana Semko <osemko@gmail.com>
2022-05-23 23:55:42 +0300

    task_02_final changes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// ------------ Create and render markup according to gallery template. --------------
+
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <li>
+    <a clsss = "gallery__item" href = "${original}">
+    <img class = "gallery__image"
+    src = "${preview}"
+    alt = "${description}"
+    />
+    </a>
+    </li>`;
+};
+
+const createGalleryMarkup = galleryItems.map(createGalleryItemMarkup).join("");
+
+const galleryElements = document.querySelector(".gallery");
+
+galleryElements.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsdata: "alt",
+  captionDelay: 250,
+});

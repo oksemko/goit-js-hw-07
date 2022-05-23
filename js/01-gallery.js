0fa69a85a3707fccdcf6aa71a5a13c124d236833
Oksana Semko <osemko@gmail.com>
2022-05-23 08:00:22 +0300

    add instance.show()

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+};
+
+const createGalleryMarkup = galleryItems.map(createGalleryItemMarkup).join("");
+
+const galleryElements = document.querySelector('.gallery');
+
+galleryElements.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) => {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.preventDefault();
+    modalShow(event.target.dataset.source);
+}
+
+
+let instance;
+function modalShow(src) {
+    instance = basicLightbox.create(
+        `
+    <div class="modal">
+        <img src="${src}" style:"height=100vh; display:block"</img>
+    </div>
+`,
+        {
+            onShow: instance => {
+                addListener();
+            }
+          onClose {
+            removeListener();
+          },  
+        },
+);
+
+instance.show();
+}
+
+

39716c69c6a74e1b1859aed1a307dcbc78798ef0
Oksana Semko <osemko@gmail.com>
2022-05-23 20:38:18 +0300

    task_01_final changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
+// console.log(galleryItems);
 
 // ------------ Create and render markup according to gallery template. --------------
 
@@ -22,60 +21,58 @@ const createGalleryItemMarkup = ({ preview, original, description }) => {
 
 const createGalleryMarkup = galleryItems.map(createGalleryItemMarkup).join("");
 
-const galleryElements = document.querySelector('.gallery');
+const galleryElements = document.querySelector(".gallery");
 
-galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup);
+galleryElements.insertAdjacentHTML("beforeend", createGalleryMarkup);
 
-galleryElements.addEventListener('click', onGalleryClick);
+galleryElements.addEventListener("click", onGalleryClick);
 
-// --------------- We check if click on 'IMG' via nodeName. ----------------------
+// ------- We check if click on 'IMG' via nodeName. Also we add preventDefault() -------
 
-function onGalleryClick(event) {
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    event.preventDefault();
-  modalShow(event.target.dataset.source);
-  
-  console.log(event.target.nodeName);
+function onGalleryClick(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  evt.preventDefault();
+  modalShow(evt.target.dataset.source);
+
+  // console.log(evt.target.nodeName);
 }
 
-
-
+// ------------ Create modalShow() according to the code from the library. ------------
 
 let instance;
 function modalShow(src) {
-    instance = basicLightbox.create(
-        `
+  instance = basicLightbox.create(
+    `
     <div class="modal">
-        <img src="${src}" style:"height=100vh; display:block"</img>
+        <img src="${src}" style="height:100vh; display:block"></img>
     </div>
 `,
-        {
-            onShow: instance => {
-                addListener();
-            }
-          onClose: instance => {
-            removeListener();
-          },  
-        },
-);
-
-instance.show();
+    {
+      onShow: (instance) => {
+        addListener();
+      },
+      onClose: (instance) => {
+        removeListener();
+      },
+    }
+  );
+  instance.show();
 }
 
+// --------------- We can close modal after press to button Escape. ------------------
+
 function addListener() {
-    window.addEventListener('keydown', onEscClick);
+  window.addEventListener("keydown", onEscClick);
 }
 
 function onEscClick(event) {
-    if (event.code === 'Esc') {
-        instance.close();
-    }
+  if (event.code === "Escape") {
+    instance.close();
+  }
 }
 
 function removeListener() {
-    window.removeEventListener ('keydown', onEscClick)
+  window.removeEventListener("keydown", onEscClick);
 }
-
-
